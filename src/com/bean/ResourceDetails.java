ae6025883626ed4680f5207695587b34f4022741
Nagendra1402 <nag1402@gmail.com>
2014-09-04 09:40:50 +0530

    Create ResourceDetails.java

---
+++ src/com/bean/ResourceDetails.java
@@ -0,0 +1,71 @@
+package com.fid.prambean;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement
+public class ResourceDetails {
+
+	private String corpId;
+	private int appId;
+	private String resourceName;
+	private String contactNumber;
+	private String designation;
+	private String reportingManager;
+	private String skillSet;
+	private int level;
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	public String getCorpId() {
+		return corpId;
+	}
+	public void setCorpId(String corpId) {
+		this.corpId = corpId;
+	}
+	public int getAppId() {
+		return appId;
+	}
+	public void setAppId(int appId) {
+		this.appId = appId;
+	}
+	public String getResourceName() {
+		return resourceName;
+	}
+	public void setResourceName(String resourceName) {
+		this.resourceName = resourceName;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getReportingManager() {
+		return reportingManager;
+	}
+	public void setReportingManager(String reportingManager) {
+		this.reportingManager = reportingManager;
+	}
+	public String getSkillSet() {
+		return skillSet;
+	}
+	public void setSkillSet(String skillSet) {
+		this.skillSet = skillSet;
+	}
+	/**
+	 * @return the contactNumber
+	 */
+	public String getContactNumber() {
+		return contactNumber;
+	}
+	/**
+	 * @param contactNumber the contactNumber to set
+	 */
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+}
