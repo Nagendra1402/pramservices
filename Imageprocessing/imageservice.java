4471b66c18842c938cd78e3c3042d00c3fb18399
Nagendra1402 <nag1402@gmail.com>
2014-10-23 17:55:30 +0530

    Update and rename imageservice to imageservice.java

--- Imageprocessing/imageservice.java
+++ Imageprocessing/imageservice.java
@@ -1,17 +1,17 @@
-        @POST
+	@POST
 	@Path("/upload")
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	public Response putImage(
-	           @FormDataParam("file") InputStream fileInputStream,
-	           @FormDataParam("file") FormDataContentDisposition contentDispositionHeader) throws SQLException{
+	           @FormDataParam("file") File file,
+	           @FormDataParam("file") FormDataContentDisposition contentDispositionHeader) throws SQLException, IOException{
 		String filePath = SERVER_UPLOAD_LOCATION_FOLDER + contentDispositionHeader.getFileName();
 		String fileName=contentDispositionHeader.getFileName();
-        detailsDao.saveFile(fileInputStream, fileName);
+		imageDao.saveFile(file, fileName);
 		 String output = "File saved to server location : " + filePath;
 		 return Response.status(200).entity(output).build();
 		
 	}
-        @GET
+	@GET
 	@Path("/download")
 	@Produces({"image/png", "image/jpeg", "image/gif"})
 	public Response getImage() throws SQLException{
@@ -20,4 +20,10 @@
 		 String output = "File saved to server location : " + filePath;
 		 return Response.status(200).entity(output).build();
 		
+	}
+	@GET
+	@Path("/getimages")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public List<ImageBean> getImageDetails(){
+		return imageDao.getImageDetails();
 	}
